74183848bede8e213c128c3695505db6f7c0e74d
Soarfz <93654235+Soarfz@users.noreply.github.com>
2022-12-13 00:50:11 +0800

    Update model.asm

--- TheScarletMoon/model.asm
+++ TheScarletMoon/model.asm
@@ -25,6 +25,7 @@ extern Player:Item
 extern Ball:Item
 extern Bricks:Item
 extern brickNum:dword
+extern existBrickNum:dword
 extern Bullets:Item
 extern InitBrickCoordX:dword
 extern InitBrickCoordY:dword
@@ -33,17 +34,124 @@ printf PROTO C :ptr DWORD, :VARARG
 calDistance PROTO C :dword, :dword, :dword, :dword
 
 .data
-
+coord sbyte "%d",0ah,0
 
 .code
 
-timer proc C id:dword
-	invoke Flush
+
+HitBrick proc C
+	pushad
+	mov edi,0
+LoopTraverseItem:
+	.if Bricks[edi].exist == 1
+		mov eax,Bricks[edi].posX
+		sub eax,40
+		mov ebx,Bricks[edi].posX
+		add ebx,40
+		mov ecx,Bricks[edi].posY
+		sub ecx,40
+		mov esi,Bricks[edi].posY
+		add esi,40
+		.if ballPosX >= eax && ballPosX <= ebx && ballPosY >= ecx && ballPosY <= esi
+			sub existBrickNum,1
+			mov Bricks[edi].exist,0
+
+			mov eax,Ball.vX
+			sub Ball.posX,eax
+			mov ebx,Ball.vY
+			sub Ball.posY,ebx
+
+			mov eax,Bricks[edi].posX
+			sub eax,40
+			mov ebx,Bricks[edi].posX
+			add ebx,40
+			.if ballPosX >= eax && ballPosX <=ebx
+				mov eax,0
+				mov ebx,Ball.vY
+				sub eax,ebx
+				mov Ball.vY,eax
+			.endif
+
+			mov eax,Bricks[edi].posY
+			sub eax,40
+			mov ebx,Bricks[edi].posY
+			add ebx,40
+			.if ballPosY >= eax && ballPosY <=ebx
+				mov eax,0
+				mov ebx,Ball.vX
+				sub eax,ebx
+				mov Ball.vX,eax
+			.endif
+		.endif
+	.endif
+	add edi,32
+	mov eax,brickNum
+	mov ebx,32
+	mul ebx
+	cmp edi,eax
+	jne LoopTraverseItem
+	jmp Finish
+Finish:
+	popad
 	ret
-timer endp
+HitBrick endp
+
+
+initBall proc C
+	mov eax,1
+	mov Ball.exist,eax
+	mov eax,20
+	mov Ball.vX,eax
+	mov eax,20
+	mov Ball.vY,eax
+	mov eax,100
+	mov ballPosX,eax
+	mov eax,400
+	mov ballPosY,eax
+initBall endp
+
+moveBall proc C
+	add Ball.vY,1
+	mov eax,Ball.vX
+	add ballPosX,eax
+	mov eax,Ball.vY
+	add ballPosY,eax
+
+	.if ballPosX <= 0
+		mov eax,0
+		mov ebx,Ball.vX
+		sub eax,ebx
+		mov Ball.vX,eax
+		mov ballPosX,0
+	.endif
+	.if ballPosX >= 760
+		mov eax,0
+		mov ebx,Ball.vX
+		sub eax,ebx
+		mov Ball.vX,eax
+		mov ballPosX,760
+	.endif
+
+	.if ballPosY <= 0
+		mov eax,0
+		mov ebx,Ball.vY
+		sub eax,ebx
+		mov Ball.vY,eax
+		mov ballPosY,0
+	.endif
+	.if ballPosY >= 560
+		mov eax,0
+		mov ebx,Ball.vY
+		sub eax,ebx
+		mov Ball.vY,eax
+		mov ballPosY,560
+	.endif
+	ret
+moveBall endp
 
 initBricks proc C
 	mov brickNum, 35
+	mov existBrickNum, 35
 	push ebx					
 	push edi
 	push ecx
@@ -72,6 +180,13 @@ initBricks proc C
 	ret
 initBricks endp
 
+timer proc C id:dword
+	invoke moveBall
+	invoke HitBrick
+	invoke Flush
+	ret
+timer endp
+
 InitGame proc C
 	pushad
 
@@ -96,7 +211,8 @@ IniPlayer:
 	mov edi,0
 IniBricks:
 	invoke initBricks
-RandLoop:
+IniBall:
+	invoke initBall
 	
 
 	invoke registerTimerEvent,offset timer
